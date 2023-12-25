d38a888a3ead68ae5cea89eb36165027c442dce1
RamchandraWarang9822 <ramchandra.warang@aissmsioit.org>
2023-12-25 13:17:31 +0530

    Day 5 Part 1 Completed

--- Day5/part1.c
+++ Day5/part1.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,7 +13,7 @@ int main() {
 
   char *line = NULL;
   size_t len = 0;
-  int result = 0;
+  unsigned long long int result = 0;
 
   // Read the seeds
   getline(&line, &len, file);
@@ -21,18 +22,19 @@ int main() {
   char *token = strtok(seedString, " ");
 
   // Convert the seed to Integer
-  int seeds[4];
+  unsigned long long int seeds[20];
   int counter = 0;
   while (token != NULL) {
-    seeds[counter++] = atoi(token);
+    seeds[counter++] = strtoull(token, NULL, 10);
     token = strtok(NULL, " ");
   }
 
+  // Using seed find the location
   for (int i = 0; i < 4; i++) {
-    int value = seeds[i];
-    printf("Current Seed: %d", value);
+    unsigned long long int value = seeds[i];
+    printf("Current Seed: %llu", value);
 
-    if (fseek(file, 3, SEEK_SET) != 0) {
+    if (fseek(file, 0, SEEK_SET) != 0) {
       perror("Error seeking file");
       fclose(file);
       return 1;
@@ -40,53 +42,47 @@ int main() {
 
     char currLine[1024];
 
+    if (fgets(currLine, sizeof(currLine), file) == NULL ||
+        fgets(currLine, sizeof(currLine), file) == NULL) {
+      perror("Error reading from file");
+      fclose(file);
+      return 1;
+    }
+
+    bool flag = false;
+
     while (fgets(currLine, sizeof(currLine), file)) {
+
       if (currLine[0] == '\n') {
         fgets(currLine, sizeof(currLine), file);
         printf("\n");
+        flag = false;
         continue;
       }
       // Convert Numbers to Integer Array
-      int dest = 0;
-      int src = 0;
-      int rng = 0;
-      sscanf(currLine, "%d %d %d", &dest, &src, &rng);
-      printf("%d %d %d\n", dest, src, rng);
+      unsigned long long int dest = 0;
+      unsigned long long int src = 0;
+      unsigned long long int rng = 0;
+      sscanf(currLine, "%llu %llu %llu", &dest, &src, &rng);
+      printf("%llu %llu %llu\n", dest, src, rng);
 
-      if (value <= src + rng && value >= src) {
-        int diff = value - src;
+      if (value <= src + rng && value >= src && flag == false) {
+        unsigned long long int diff = value - src;
         value = dest + diff;
-        printf("Value changed to: %d\n", value);
+        printf("Value changed to: %llu\n", value);
+        flag = true;
       }
     }
 
     if (result == 0) {
       result = value;
-    } else if (value < result) {
+    } else if (result > value) {
       result = value;
     }
+    printf("End of the loop Result :%llu\n", result);
   }
 
-  printf("Final Result : %d\n", result);
-
-  // Reading Source , Destination and Range
-  // char currLine[1024];
-  // while (fgets(currLine, sizeof(currLine), file)) {
-  //   if (currLine[0] == '\n') {
-  //     fgets(currLine, sizeof(currLine), file);
-  //     printf("\n");
-  //     continue;
-  //   }
-  //   // Convert Numbers to Integer Array
-  //   int nums[3];
-  //   sscanf(currLine, "%d %d %d", &nums[0], &nums[1], &nums[2]);
-  //
-  //   // Print the Integer Array
-  //   for (int i = 0; i < 3; i++) {
-  //     printf("%d ", nums[i]);
-  //   }
-  //   printf("\n");
-  // }
+  printf("Final Result : %llu\n", result);
 
   fclose(file);
   return 0;
